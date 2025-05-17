1495009d8412b64056bd7a9856fb63e5ee39f963
Yawksh <yawukalfentaw@gmail.com>
2025-05-17 20:52:18 +0300

    return

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -11,9 +11,11 @@ def stream_users_in_batches(batch_size):
         yield rows
     cursor.close()
     connection.close()
+    return
 
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
                 print(user)
+    return
