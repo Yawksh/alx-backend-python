59071e5e8043864ca0761a88a2a82f1e91923632
Yawksh <yawukalfentaw@gmail.com>
2025-06-02 23:05:21 +0300

    view

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from .models import User, Message, Conversation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'content', 'timestamp']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'created_at', 'messages']
+
 
 # Create your views here.
