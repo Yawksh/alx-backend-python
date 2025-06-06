e8176cabeec8e3764dcb6630418a012b9c710b87
Yawksh <yawukalfentaw@gmail.com>
2025-06-06 21:56:16 +0300

    routers

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 
