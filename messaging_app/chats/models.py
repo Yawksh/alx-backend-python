4e2969aa37e959059182ba72adc61cf03ad11e8d
Yawksh <yawukalfentaw@gmail.com>
2025-06-06 21:32:13 +0300

    model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,14 +6,8 @@ import uuid
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(
-    max_length=15,
-    unique=True,
-    null=True,  # Temporary, remove after migration
-    blank=True,  # Temporary, remove after migration
-    # or add a proper default if appropriate
-    default="0000000000"
-)
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=128)
 
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
     USERNAME_FIELD = 'email'
@@ -35,7 +29,7 @@ class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     message_body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.sender.email} @ {self.sent_at}"
