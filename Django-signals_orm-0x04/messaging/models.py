01bd631247cc5b1c8a49bbe8100d16698ca346b0
Yawksh <yawukalfentaw@gmail.com>
2025-06-15 23:35:42 +0300

    new edit

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -16,4 +16,4 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='histories')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
-    
+    edited_by = models.ForeignKey(User,on_delete  = models.CASCADE,related_name= 'updated_by')
