1f3ae66190c34b9fbc0272511b78c392d3957e15
Yawksh <yawukalfentaw@gmail.com>
2025-06-29 12:41:46 +0300

    dependency

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -5,3 +5,19 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+from envparse import env
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env.read_envfile(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env.str('MYSQL_DATABASE'),
+        'USER': env.str('MYSQL_USER'),
+        'PASSWORD': env.str('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
